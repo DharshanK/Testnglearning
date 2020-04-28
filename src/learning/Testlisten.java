17e67b143b8e8951af289bc612e3bcebb1f1ec8a
dharshan <k.dharshan29@gmail.com>
2020-04-28 21:43:03 +0530

    learning code

---
+++ src/learning/Testlisten.java
@@ -0,0 +1,56 @@
+package learning;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class Testlisten implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Testcase Strated Execution" +result.getName());
+		Reporter.log("<a href=\"C:\\quote.jpg\" target=\"_blank\">Test case Started :</a>" +result.getName()) ;
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ src/learning/econtrol.java
@@ -0,0 +1,12 @@
+package learning;
+
+import org.testng.annotations.Test;
+
+public class econtrol {
+	@Test
+	public void nmod()
+	{
+		System.out.println("New module running");
+	}
+
+}
